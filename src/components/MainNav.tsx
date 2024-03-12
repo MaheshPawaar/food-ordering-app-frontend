b9edbc85b9927618eb43223f56388e3f59d4b504
Mahesh Pawar <pawarmahesh723@gmail.com>
2024-03-13 01:02:29 +0530

    Code Refactor

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,6 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from './ui/button';
-import { spawn } from 'child_process';
 import UsernameMenu from './UsernameMenu';
 
 const MainNav = () => {
