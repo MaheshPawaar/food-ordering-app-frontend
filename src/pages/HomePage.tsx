ed1d37745864422734155f9c9634b65eab44fbfd
Mahesh Pawar <pawarmahesh723@gmail.com>
2024-03-19 00:06:15 +0530

    Implement SearchBar component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,37 @@
-import LandingImage from '../assets/landing.png'
-import AppDownloadImage from '../assets/appDownload.png'
+import LandingImage from '../assets/landing.png';
+import AppDownloadImage from '../assets/appDownload.png';
+import SearchBar, { SearchForm } from '@/components/SearchBar';
+import { useNavigate } from 'react-router-dom';
+
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
+
   return (
     <div className="flex flex-col gap-12">
-      <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+      <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
         <h1 className="text-5xl font-bold tracking-tight text-orange-600">
           Tuck into a takeaway today
         </h1>
         <span className="text-xl">Food is just a click away!</span>
+        <SearchBar placeholder="Search by City or Town" onSubmit={handleSearchSubmit} />
       </div>
       <div className="grid md:grid-cols-2 gap-5">
-        <img src={LandingImage}/>
-        <div className='flex flex-col items-center justify-center gap-4 text-center'>
-          <span className="font-bold text-3xl tracking-tight">Order takeaway even faster!</span>
-          <span>Download the LetsEat app for faster ordering and personalised recommendations</span>
-          <img src={AppDownloadImage}/>
+        <img src={LandingImage} />
+        <div className="flex flex-col items-center justify-center gap-4 text-center">
+          <span className="font-bold text-3xl tracking-tight">
+            Order takeaway even faster!
+          </span>
+          <span>
+            Download the LetsEat app for faster ordering and personalised
+            recommendations
+          </span>
+          <img src={AppDownloadImage} />
         </div>
       </div>
     </div>
