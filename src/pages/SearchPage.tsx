8c77216c35669609b20d8a6feb74335a32e05775
Mahesh Pawar <pawarmahesh723@gmail.com>
2024-03-19 16:10:29 +0530

    Implement search api request

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,9 +1,22 @@
+import { useSearchRestaurants } from '@/api/RestaurantApi';
 import { useParams } from 'react-router-dom';
 
 const SearchPage = () => {
   const { city } = useParams();
+  const { results } = useSearchRestaurants(city);
 
-  return <span>User searched for {city} </span>;
+  return (
+    <span>
+      User searched for {city}{" "}
+      <span>
+        {results?.data.map((restaurant) => (
+          <span>
+            found - {restaurant.restaurantName}, {restaurant.city}
+          </span>
+        ))}
+      </span>
+    </span>
+  );
 };
 
 export default SearchPage;
