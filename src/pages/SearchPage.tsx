345ebd2ca18efb40b62a90e9fe8d0ab9779eb1f0
Mahesh Pawar <pawarmahesh723@gmail.com>
2024-03-21 13:12:14 +0530

    Implement Cuisine filter

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,4 +1,5 @@
 import { useSearchRestaurants } from '@/api/RestaurantApi';
+import CuisineFilter from '@/components/CuisineFilter';
 import PaginationSelector from '@/components/PaginationSelector';
 import SearchBar, { SearchForm } from '@/components/SearchBar';
 import SearchResultInfo from '@/components/SearchResultInfo';
@@ -9,6 +10,7 @@ import { useParams } from 'react-router-dom';
 export type SearchState = {
   searchQuery: string;
   page: number;
+  selectedCuisines: string[];
 };
 
 const SearchPage = () => {
@@ -16,8 +18,20 @@ const SearchPage = () => {
   const [searchState, setSearchState] = useState<SearchState>({
     searchQuery: '',
     page: 1,
+    selectedCuisines: [],
   });
+
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
   const { results, isLoading } = useSearchRestaurants(searchState, city);
+
+  const setSelectedCuisines = (selectedCuisines: string[]) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      selectedCuisines,
+      page: 1,
+    }));
+  };
+
   const setPage = (page: number) => {
     setSearchState((prevState) => ({
       ...prevState,
@@ -48,8 +62,17 @@ const SearchPage = () => {
   }
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-[250_1fr] gap-5">
-      <div id="cuisines-list">{/* Insert cuisines list here */}</div>
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div id="cuisines-list">
+        <CuisineFilter
+          selectedCuisines={searchState.selectedCuisines}
+          onChange={setSelectedCuisines}
+          isExpanded={isExpanded}
+          onExpandedClick={() =>
+            setIsExpanded((prevIsExpanded) => !prevIsExpanded)
+          }
+        />
+      </div>
       <div id="main-content" className="flex flex-col gap-5">
         <SearchBar
           searchQuery={searchState.searchQuery}
@@ -59,7 +82,7 @@ const SearchPage = () => {
         />
         <SearchResultInfo total={results.pagination.total} city={city} />
         {results.data.map((restaurant) => (
-          <SearchResultsCard key={restaurant._id} restaurant={restaurant} />
+          <SearchResultsCard restaurant={restaurant} />
         ))}
         <PaginationSelector
           page={results.pagination.page}
