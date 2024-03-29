7c516d0dab3096c2c317dc86f8be65ca9bca731f
Mahesh Pawar <pawarmahesh723@gmail.com>
2024-03-29 17:46:16 +0530

    Implement restaurant orders page

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,8 +1,11 @@
 import {
   useCreateMyRestaurant,
   useGetMyRestaurant,
+  useGetMyRestaurantOrders,
   useUpdateMyRestaurant,
 } from '@/api/MyRestaurantAPI';
+import OrderItemCard from '@/components/OrderItemCard';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import ManageRestaurantForm from '@/forms/manage-restaurant-form/ManageRestaurantForm';
 
 const ManageRestaurantPage = () => {
@@ -10,16 +13,34 @@ const ManageRestaurantPage = () => {
     useCreateMyRestaurant();
   const { updateRestaurant, isLoading: isUpdateLoading } =
     useUpdateMyRestaurant();
+  const { orders } = useGetMyRestaurantOrders();
   const { restaurant } = useGetMyRestaurant();
 
   const isEditing = !!restaurant;
 
   return (
-    <ManageRestaurantForm
-      restaurant={restaurant}
-      onSave={isEditing ? updateRestaurant : createRestaurant}
-      isLoading={isCreateLoading || isUpdateLoading}
-    />
+    <Tabs defaultValue="orders">
+      <TabsList>
+        <TabsTrigger value="orders">Orders</TabsTrigger>
+        <TabsTrigger value="manage-restaurant">Manage Restaurant</TabsTrigger>
+      </TabsList>
+      <TabsContent
+        value="orders"
+        className="space-y-5 bg-gray-50 p-10 rounded-lg"
+      >
+        <h2 className="text-2xl font-bold">{orders?.length} active orders</h2>
+        {orders?.map((order) => (
+          <OrderItemCard order={order} />
+        ))}
+      </TabsContent>
+      <TabsContent value="manage-restaurant">
+        <ManageRestaurantForm
+          restaurant={restaurant}
+          onSave={isEditing ? updateRestaurant : createRestaurant}
+          isLoading={isCreateLoading || isUpdateLoading}
+        />
+      </TabsContent>
+    </Tabs>
   );
 };
 
